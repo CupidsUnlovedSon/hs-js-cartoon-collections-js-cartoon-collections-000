3879e360fedb391a1cb41732775a2195cc095d0e
Wayne Thompson <wj.thompson89@gmail.com>
2020-01-23 10:47:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function dwarfRollCall(dwarves) {
-}
+  //1
+  var dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
 
-function summonCaptainPlanet(planeteerCalls){
-}
+  function dwarfRollCall(dwarves){
+   var array =[];
+   for (var i=0;i<dwarves.length;i++){
+     array.push(`${i+1}. ${dwarves[i]} `);
+   }
+   return array.join("");
+  }
 
-function longPlaneteerCalls(words) {
-}
+  //2
+  var planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
 
-function findTheCheese (foods) {
-}
+  function summonCaptainPlanet(planeteerCalls){
+    return planeteerCalls.map(a=>a.toUpperCase()+"!");
+  }
+
+  //3
+  var shortWords = ["wind", "fire"];
+  var assortedWords = ["earth", "wind", "fire", "water", "heart"];
+
+  function longPlaneteerCalls(words){
+    for(var i=0;i<words.length;i++){
+      if (words[i].length>4){
+        return true
+      }
+    }
+    return false
+  }
+
+  var snacks = ["crackers", "gouda", "thyme"];
+  var soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"];
+  var ingredients = ["garlic", "rosemary", "bread"];
+
+  //4
+  function findTheCheese (foods) {
+    for(var i=0;i<foods.length;i++){
+      if (foods[i]=="cheddar"
+         ||foods[i]=="gouda"
+         ||foods[i]=="camembert"
+         ){
+        return foods[i]
+      }
+    }
+    return "no cheese!"
+  }
